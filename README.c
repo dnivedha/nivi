952b6e6758f9129b47b321e7b2a0b1318255dfb6
dnivedha <nivethadurairaju95@gmail.com>
2016-07-08 11:36:03 +0530

    finish

---
+++ README.c
@@ -0,0 +1,19 @@
+# nivi
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the no:");
+scanf("%d",&n);
+if(n>0)
+{
+printf("the no is pos",n);
+elseif(n<0)
+{
+printf("the no is neg",n);
+else
+printf("the no is zero",n);
+}
+}
+}
